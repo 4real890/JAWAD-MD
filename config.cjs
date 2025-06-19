abb69366e1666d88b728106c81c8dd545c446677
4real890 <handsdesouth@gmail.com>
2025-06-20 02:19:12 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "IK~sD0mTAIT#0BeaFiaK7vcxPStJnnfHIoLbKfGAtYcLKJ4cqz1ykbU",
+  SESSION_ID: process.env.SESSION_ID || "IK~RHE31I4I#__syC-ZHy7-XemkNTJo-s4W6Ql58eyQycBkZVj3GwGA",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
